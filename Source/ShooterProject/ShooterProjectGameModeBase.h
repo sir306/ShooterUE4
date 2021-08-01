ec2c994b5717d174da68fc8769788c5e82164ba3
sir306 <40708936+sir306@users.noreply.github.com>
2021-08-01 19:09:52 +1200

    Initial commit

---
+++ Source/ShooterProject/ShooterProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERPROJECT_API AShooterProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
